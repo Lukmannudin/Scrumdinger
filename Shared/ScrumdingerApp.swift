baef15eb4db110c166938e9c23c8334e6a4a0cf1
lukmannudin <lukmannudin.priatna@happyfresh.com>
2021-07-18 19:15:12 +0700

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by HF on 18/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
